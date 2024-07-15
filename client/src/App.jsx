eaf6f0ebb9709dd98e98c21d01342515da1662a2
farheen-syed1 <syedfarheensf1@hotmail.com>
2024-07-15 00:06:22 -0400

    final push

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Pages/Home/Home'
-import Footer from './Components/Footer/Footer';
-import Navbar from './Components/Header/Header';
-import Bmi from './Pages/IBM/Bmi';
-import Exercise from './Pages/Exercise/Exercise';
-import HealthRecipes from './Pages/Health-Recipes/HealthRecipes';
-import Register from './Pages/Register/Register';
-import Login from './Pages/Login/Login';
-
+import Home from "./Pages/Home/Home";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Header/Header";
+import Bmi from "./Pages/BMI/Bmi";
+import Exercise from "./Pages/Exercise/Exercise";
+import HealthRecipes from "./Pages/Health-Recipes/HealthRecipes";
+import Register from "./Pages/Register/Register";
+import Login from "./Pages/Login/Login";
 
 function App() {
-
-
   const [exerciseDetails, setExerciseDetails] = useState();
   const [exerciseData, setExerciseData] = useState([]);
 
@@ -23,76 +20,66 @@ function App() {
 
   return (
     <>
-    
-    
-
-    
-    
       <BrowserRouter>
-      
-      <Navbar />
-      <Routes>
-   
-        <Route path="/" element={<Home/>} />
-        <Route path="/exercise"
-          element={
-            <Exercise
-              exerciseDetails={exerciseDetails}
-              setExerciseDetails={setExerciseDetails}
-              exerciseData={exerciseData}
-              setExerciseData={setExerciseData}
-          
-            />
-          }
-        />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/exercise"
+            element={
+              <Exercise
+                exerciseDetails={exerciseDetails}
+                setExerciseDetails={setExerciseDetails}
+                exerciseData={exerciseData}
+                setExerciseData={setExerciseData}
+              />
+            }
+          />
 
-        <Route path="/exercise/:id"
-          element={
-            <Exercise
-            exerciseDetails={exerciseDetails}
-              setExerciseDetails={setExerciseDetails}
-              exerciseData={exerciseData}
-              setExerciseData={setExerciseData}
-            />
-          }
-        />
+          <Route
+            path="/exercise/:id"
+            element={
+              <Exercise
+                exerciseDetails={exerciseDetails}
+                setExerciseDetails={setExerciseDetails}
+                exerciseData={exerciseData}
+                setExerciseData={setExerciseData}
+              />
+            }
+          />
 
-<Route path="/health-recipes"
-          element={
-            <HealthRecipes
-              recipeDetails={recipeDetails}
-              setRecipeDetails={setRecipeDetails}
-              recipeData={recipeData}
-              setRecipeData={setRecipeData}
-          
-            />
-          }
-        />
+          <Route
+            path="/health-recipes"
+            element={
+              <HealthRecipes
+                recipeDetails={recipeDetails}
+                setRecipeDetails={setRecipeDetails}
+                recipeData={recipeData}
+                setRecipeData={setRecipeData}
+              />
+            }
+          />
 
-        <Route path="/health-recipes/:id"
-          element={
-            <HealthRecipes
-              recipeDetails={recipeDetails}
-              setRecipeDetails={setRecipeDetails}
-              recipeData={recipeData}
-              setRecipeData={setRecipeData}
-            />
-          }
-        />
+          <Route
+            path="/health-recipes/:id"
+            element={
+              <HealthRecipes
+                recipeDetails={recipeDetails}
+                setRecipeDetails={setRecipeDetails}
+                recipeData={recipeData}
+                setRecipeData={setRecipeData}
+              />
+            }
+          />
 
-
-
-
-        <Route path="/bmi" element={<Bmi/>} />
-        <Route path="/signUp" element={<Register/>} />
-        <Route path="/login" element={<Login/>} />
-      </Routes>
-   <Footer/>
-    </BrowserRouter>
-
-
+          <Route path="/bmi" element={<Bmi />} />
+          <Route path="/signUp" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
